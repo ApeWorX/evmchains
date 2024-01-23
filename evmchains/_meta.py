c71c7069cc9052ddf54e9f7ed81d93f8a63ad99a
Mike Shultz <shultzm@gmail.com>
2024-01-23 11:58:42 -0700

    fix: dependency spec was invalid

--- evmchains/_meta.py
+++ evmchains/_meta.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.1a1'
-__version_tuple__ = version_tuple = (0, 0, 1)
+__version__ = version = '0.1.dev4+g34f2bde.d20240123'
+__version_tuple__ = version_tuple = (0, 1, 'dev4', 'g34f2bde.d20240123')
