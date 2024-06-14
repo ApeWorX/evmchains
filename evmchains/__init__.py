72b29478319523e3cf15afef95906c774526a310
Mike Shultz <mike@mikeshultz.com>
2024-06-14 09:19:40 -0600

    feat: support env var replacements and filter out rpc endpoints when a replacement is unavailable

--- evmchains/__init__.py
+++ evmchains/__init__.py
@@ -1,7 +1,12 @@
+import os
 import random
+import re
+from typing import List
 
 from evmchains.chains import PUBLIC_CHAIN_META
 from evmchains.types import Chain
+
+ENV_VAR_REGEX = re.compile(r"\$\{([A-Za-z0-9_]+)\}")
 
 
 def get_chain_meta(ecosystem: str, network: str) -> Chain:
@@ -15,10 +20,31 @@ def get_chain_meta(ecosystem: str, network: str) -> Chain:
     return Chain(**PUBLIC_CHAIN_META[ecosystem][network])
 
 
+def get_rpcs(ecosystem: str, network: str) -> List[str]:
+    """Get a list of valid RPC endpoints for an ecosystem:network pair"""
+    rpcs = []
+
+    chain = get_chain_meta(ecosystem, network)
+
+    for rpc in chain.rpc:
+        # Look for env var replacements in the endpoint URL
+        match = ENV_VAR_REGEX.search(rpc)
+        if match:
+            to_replace = match.group(0)
+            env_var = match.group(1)
+            # Include them only if the env var is available
+            if env_var in os.environ:
+                rpcs.append(rpc.replace(to_replace, os.environ[env_var]))
+        else:
+            rpcs.append(rpc)
+
+    return rpcs
+
+
 def get_random_rpc(ecosystem: str, network: str) -> str:
     """Return a random RPC endpoint for an ecosystem:network pair"""
-    chain = get_chain_meta(ecosystem, network)
-    return random.choice(chain.rpc)
+    rpcs = get_rpcs(ecosystem, network)
+    return random.choice(rpcs)
 
 
-__all__ = ["PUBLIC_CHAIN_META", "Chain", "get_chain_meta"]
+__all__ = ["PUBLIC_CHAIN_META", "Chain", "get_chain_meta", "get_random_rpc", "get_rpcs"]
