a6f6c5a18d4e1a996bafe299a2bc0136c95c5c17
Mike Shultz <mike@apeworx.io>
2024-06-14 10:12:55 -0600

    feat: support env var replacements  (#16)

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,4 +1,26 @@
-from evmchains import PUBLIC_CHAIN_META, Chain, get_chain_meta
+import os
+from contextlib import contextmanager
+
+import pytest
+
+from evmchains import PUBLIC_CHAIN_META, Chain, get_chain_meta, get_rpcs
+
+
+@pytest.fixture()
+def env():
+    @contextmanager
+    def inner(environ=dict(), unset=list()):
+        old_environ = os.environ.copy()
+        os.environ.update(environ)
+        for key in unset:
+            try:
+                del os.environ[key]
+            except KeyError:
+                pass
+        yield
+        os.environ = old_environ
+
+    return inner
 
 
 def test_chain_structure():
@@ -26,3 +48,41 @@ def test_get_chain_meta():
     assert chain.name == "Arbitrum One"
     assert chain.nativeCurrency["symbol"] == "ETH"
     assert len(chain.explorers) > 0
+
+
+def test_get_rpcs_alchemy(env):
+    alchemy_key = "alchemistsayswhat"
+    expected_rpc = f"https://arb-mainnet.g.alchemy.com/v2/{alchemy_key}"
+
+    def find_rpc(rpcs, expected):
+        for rpc in rpcs:
+            if rpc == expected:
+                return True
+        return False
+
+    with env({"ALCHEMY_API_KEY": alchemy_key}, unset=["INFURA_API_KEY"]):
+        rpcs = get_rpcs("arbitrum", "mainnet")
+        assert isinstance(rpcs, list)
+        assert len(rpcs) > 0
+        assert find_rpc(rpcs, expected_rpc)
+        # Ensure the infura endpoint was also filtered out
+        assert len([rpc for rpc in rpcs if "infura" in rpc]) == 0
+
+
+def test_get_rpcs_infura(env):
+    infura_key = "infuraaaaaa"
+    expected_rpc = f"https://arbitrum-mainnet.infura.io/v3/{infura_key}"
+
+    def find_rpc(rpcs, expected):
+        for rpc in rpcs:
+            if rpc == expected:
+                return True
+        return False
+
+    with env({"INFURA_API_KEY": infura_key}, unset=["ALCHEMY_API_KEY"]):
+        rpcs = get_rpcs("arbitrum", "mainnet")
+        assert isinstance(rpcs, list)
+        assert len(rpcs) > 0
+        assert find_rpc(rpcs, expected_rpc)
+        # Ensure the alchemy endpoint was also filtered out
+        assert len([rpc for rpc in rpcs if "alchemy" in rpc]) == 0
